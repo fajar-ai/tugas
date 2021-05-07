1229fd246bba9450bcb9848bcdb15dfa0fe49518
fajar-ai <fajar2682003@gmail.com>
2021-05-07 16:00:21 +0700

    reverse-letter-in-string

---
+++ ReverseletterInString.php
@@ -0,0 +1,19 @@
+<?php 
+	
+// Tugas Fajar: 
+// Judul: Reverse letter In String
+// Contoh: input= "kamu", output = "umak"
+// Catatan: tidak boleh pake fungsi yang sudah tersediadidalam php
+// Petunjuk: Guanakan Looping
+// Kriteria: Kerjakan dalam 4 versi looping (for, foreach,while,do-wihle)
+	
+$input = "kamu";
+	echo strrev($input);
+
+
+
+
+
+
+
+?>
